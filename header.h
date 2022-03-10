dc2ad3f712df82fbba6477f3d9b113b1a2a4d69f
TangfeiLi <litangfei@sjtu.edu.cn>
2022-03-10 13:35:48 +0800

    Second commit

---
+++ header.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <exception>
+#include "mysql.h"
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::cin;
+using std::printf;
+using std::exception;
